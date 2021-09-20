49bce692ec65c5849538d9181e861d620910ffb0
Shawn Su <shawnsu@developersinstitute.co.nz>
2021-09-21 07:29:09 +1200

    fix: bug that prevents certain date string to be recognised

--- server.js
+++ server.js
@@ -2,75 +2,68 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
 
 // endpoint for date
 app.get("/api/:date?", (req, res) => {
   const date = req.params.date;
-  console.log('📭', date)
-  let output;
-  const dateReg = new RegExp(/^\d{1,4}-\d{1,2}-\d{1,2}$/);
+  // console.log("📭", date);
+  let output, unix, utc;
   const unixReg = new RegExp(/^\d+$/);
-  // const utc = (new Date(date)).toString();
-  // if (utc === "Invalid Date") res.send({ 'error': "Invalid Date" })
   const buildOutput = (unix, utc) => {
-    if (utc === "Invalid Date") return { 'error': "Invalid Date" }
+    if (utc === "Invalid Date") return { error: "Invalid Date" };
     else {
       // ! not very elegant, refactor
-      utc = utc.substr(0, 3) + ', ' + utc.substr(8, 3) + utc.substr(4, 4) + utc.substr(11, 17);
-      return { unix, utc }
+      utc =
+        utc.substr(0, 3) +
+        ", " +
+        utc.substr(8, 3) +
+        utc.substr(4, 4) +
+        utc.substr(11, 17);
+      return { unix, utc };
     }
-  }
+  };
 
   // for whatever reason, when date param is left empty, res.params.date becomes undefined instead of ''
   if (!date) {
-    const unix = Date.now()
-    const utc = (new Date(unix)).toString()
-    output = buildOutput(unix, utc)
-  } else if (dateReg.test(date)) {
-    const unix = Date.parse(date)
-    const utc = (new Date(date)).toString()
-    output = buildOutput(unix, utc)
+    //if input is empty
+    unix = Date.now();
+    utc = new Date(unix).toString();
   } else if (unixReg.test(date)) {
-    const unix = Number(date)
-    const utc = (new Date(unix)).toString()
-    output = buildOutput(unix, utc)
+    //if input is in unix format
+    unix = Number(date);
+    utc = new Date(unix).toString();
   } else {
-    output = { 'error': "Invalid Date" }
+    unix = Date.parse(date);
+    utc = new Date(date).toString();
   }
 
-  res.send(output)
-})
-
-// endpoint for unix
-
-
-// listen for requests :)
-// var listener = app.listen(process.env.PORT , function () {
-//   console.log('Your app is listening on port ' + listener.address().port);
-// });
-app.listen(5000, function () {
-  console.log('Your app is listening on port 5000');
+  output = buildOutput(unix, utc);
+  res.send(output);
 });
 
-module.exports = app
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
+
+module.exports = app;
